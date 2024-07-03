d4bb8d532d2c294499efc57993a916d8e322a7a4
太田百音 <otamomone@momomac.local>
2024-07-03 23:27:41 +0900

    継承

---
+++ iphone/src/iphone/Iphone.java
@@ -0,0 +1,16 @@
+package iphone;
+
+public class Iphone {
+	public static void main(String[] aregs) {
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+		iphone.call();
+		iphone.mail();
+		iphone.photo();
+		iphone.internet();
+	}
+
+}
